7ac5dd58472934775eba979c9722a679901bf59e
thuteshubham <48601388+thuteshubham@users.noreply.github.com>
2024-02-16 15:50:16 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,28 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [React.createElement(
+    "div",
+    { id: "child" },[
+     React.createElement("h1", {}, "I am H1 tag"),
+     React.createElement("h2", {}, "I am H2 tag")
+    ]
+  ),
+  React.createElement(
+    "div",
+    { id: "child2" },[
+     React.createElement("h1", {}, "I am H1 tag"),
+     React.createElement("h2", {}, "I am H2 tag")
+    ]
+  )]
+);
+
+const headers = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello world from react!"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
