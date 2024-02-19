e6052ec52952eaea4cf1ecfc2edaa9fd7201b67d
thuteshubham <48601388+thuteshubham@users.noreply.github.com>
2024-02-19 17:31:40 +0530

    Add files via upload
    episode 2

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
   [React.createElement(
     "div",
     { id: "child" },[
-     React.createElement("h1", {}, "I am H1 tag"),
-     React.createElement("h2", {}, "I am H2 tag")
+     React.createElement("h1", {}, "This is namaste react"),
+     React.createElement("h2", {}, "I am H2 tagd1")
     ]
   ),
   React.createElement(
---
+++ README.md
@@ -0,0 +1,8 @@
+# Namaste react 
+
+#Parcel
+ - Dev build
+ - Local server 
+ - HMR =  hot module replacement
+ - It Uses File wathcing algorithm writen in C++
+ - faster build becuase of caching
